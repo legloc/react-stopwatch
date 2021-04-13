b0a26f4af58fcca7f7e13a5fb90868ab8b227f9d
legloc <developer.aliev@gmail.com>
2021-04-13 10:05:04 +0300

    Created template

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import './App.sass'
 
 const App = () => {
   return (
-    <div>
+    <div className="wrapper">
+      
+      <span className="time">00:00:00</span>
+
+      <div className="controls">
+        <button className="control btn-wait">
+          <ion-icon name="pause" />
+        </button>
+        <button className="control btn-play">
+          <ion-icon name="play" />
+        </button>
+        <button className="control btn-reset">
+          <ion-icon name="refresh" />
+        </button>
+      </div>
+
     </div>
   )
 }
